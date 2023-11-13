0630e592f00f53efb0e59048c3246a4d0f2bdc25
rluki99 <120097849+rluki99@users.noreply.github.com>
2023-11-13 23:38:45 +0100

    feat: using Leaflet library to render map

--- script.js
+++ script.js
@@ -14,9 +14,23 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
-        const {latitude} = position.coords
-        const {longitude} = position.coords
-        console.log(`https://www.google.pl/maps/@${latitude},${longitude}`);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.pl/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude]
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');
