77e4241ffabc3f659445a6b9716cca0e085e9376
rluki99 <lukasz.beczek24@gmail.com>
2023-11-14 10:59:45 +0100

    feat: displaying marker on map

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ if (navigator.geolocation)
       const { longitude } = position.coords;
       console.log(`https://www.google.pl/maps/@${latitude},${longitude}`);
 
-      const coords = [latitude, longitude]
+      const coords = [latitude, longitude];
 
       const map = L.map('map').setView(coords, 13);
 
@@ -27,10 +27,22 @@ if (navigator.geolocation)
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
+      map.on('click', (mapEvent) => {
+        console.log(mapEvent);
+        const {lat, lng} = mapEvent.latlng
+
+        L.marker([lat, lng])
         .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .bindPopup(L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        }))
+        .setPopupContent('Workout')
         .openPopup();
+      });
     },
     function () {
       alert('Could not get your position');
