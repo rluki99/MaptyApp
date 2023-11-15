8579d5ff6048f48119834b9b1afdd1586f1ba0dd
rluki99 <lukasz.beczek24@gmail.com>
2023-11-15 11:41:08 +0100

    feat: working with localStorage

--- script.js
+++ script.js
@@ -81,7 +81,13 @@ class App {
   #workouts = [];
 
   constructor() {
+    // get user's position
     this._getPosition();
+
+    // get data from local storage
+    this._getLocalStorage()
+
+    // attach event handlers
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
     containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
@@ -112,6 +118,10 @@ class App {
 
     // handling clicks on map
     this.#map.on('click', this._showForm.bind(this));
+
+    this.#workouts.forEach(work => {
+      this._renderWorkoutMarker(work)
+    })
   }
 
   _showForm(mapE) {
@@ -184,7 +194,6 @@ class App {
 
     // add new object to workout array
     this.#workouts.push(workout);
-    console.log(workout);
 
     // render workout on map as marker
     this._renderWorkoutMarker(workout);
@@ -194,6 +203,9 @@ class App {
 
     // hide form + clear input fields
     this._hideForm();
+
+    // set local storage to all workouts
+    this._setLocalStorage()
   }
 
   _renderWorkoutMarker(workout) {
@@ -279,8 +291,29 @@ class App {
     })
 
     // using the public interface
-    workout.click()
-    console.log(workout);
+    // workout.click()
+  }
+
+  _setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts))
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('workouts'))
+
+    if(!data) return
+
+    this.#workouts = data
+
+    this.#workouts.forEach(work => {
+      this._renderWorkout(work)
+    })
+  }
+
+  // method to reset localStorage for example via console
+  reset() {
+    localStorage.removeItem('workouts')
+    location.reload()
   }
 }
 
